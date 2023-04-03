5bfb25c42a5d366fe47f04f43e369409cb2136dc
Anastasia <Anastasia@DESKTOP-BEQ3HLM>
2023-04-03 21:54:56 +0300

    mm

--- chuc_coursework/Create.Designer.cs
+++ chuc_coursework/Create.Designer.cs
@@ -52,13 +52,13 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label15 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.label16 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
-            this.label18 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -133,7 +133,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1177, 498);
+            this.button1.Location = new System.Drawing.Point(1134, 473);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(67, 27);
             this.button1.TabIndex = 8;
@@ -145,7 +145,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(55, 329);
+            this.label5.Location = new System.Drawing.Point(55, 336);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(57, 20);
             this.label5.TabIndex = 19;
@@ -274,57 +274,34 @@
             this.textBox7.Size = new System.Drawing.Size(212, 20);
             this.textBox7.TabIndex = 36;
             // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(344, 171);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(212, 20);
-            this.textBox8.TabIndex = 37;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(267, 473);
+            this.button2.Location = new System.Drawing.Point(489, 254);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 27);
             this.button2.TabIndex = 38;
-            this.button2.Text = "обновить";
+            this.button2.Text = "очистить";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label16
-            // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(118, 338);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(41, 13);
-            this.label16.TabIndex = 39;
-            this.label16.Text = "label16";
-            this.label16.Click += new System.EventHandler(this.label16_Click);
             // 
             // label17
             // 
             this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label17.Location = new System.Drawing.Point(118, 364);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(41, 13);
+            this.label17.Size = new System.Drawing.Size(67, 20);
             this.label17.TabIndex = 40;
             this.label17.Text = "label17";
             // 
-            // label18
-            // 
-            this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(118, 400);
-            this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(41, 13);
-            this.label18.TabIndex = 41;
-            this.label18.Text = "label18";
-            // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(589, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(655, 342);
+            this.listBox1.Size = new System.Drawing.Size(748, 224);
             this.listBox1.TabIndex = 42;
             // 
             // button3
@@ -337,19 +314,47 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(344, 171);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(212, 21);
+            this.comboBox3.TabIndex = 44;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(118, 336);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 20);
+            this.label8.TabIndex = 45;
+            this.label8.Text = "label8";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(118, 401);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(67, 20);
+            this.label14.TabIndex = 46;
+            this.label14.Text = "label14";
+            // 
             // Create
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BurlyWood;
             this.ClientSize = new System.Drawing.Size(1432, 631);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label18);
             this.Controls.Add(this.label17);
-            this.Controls.Add(this.label16);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.textBox6);
@@ -409,12 +414,12 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label17;
-        private System.Windows.Forms.Label label18;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label14;
     }
 }
--- chuc_coursework/Create.cs
+++ chuc_coursework/Create.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,6 +24,7 @@ namespace chuc_coursework
         {
             Toll();
             Po();
+            Vo();
         }
         public void Toll()
         {
@@ -36,6 +38,7 @@ namespace chuc_coursework
             conn.Close();
             reader.Close();
         }
+
         public void Po()
         {
             MySqlConnection conn = new MySqlConnection(authorization.connStr);
@@ -49,19 +52,31 @@ namespace chuc_coursework
             reader.Close();
         }
 
+        public void Vo()
+        {
+            MySqlConnection conn = new MySqlConnection(authorization.connStr);
+            conn.Open();
+            string sql = "SELECT nds FROM NDS";
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                comboBox3.Items.Add(reader[0].ToString());
+            conn.Close();
+            reader.Close();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             MySqlConnection conn = new MySqlConnection(authorization.connStr);
             conn.Open();
             MySqlCommand command = new MySqlCommand("INSERT INTO ReceiptInvoice (date,number,dateShipment,provider,retailSum,incomingSum) values (@date,@number,@dateShipment,@provider,@retailSum,@incomingSum)");
             command.Connection = conn;
-            //command.Parameters.AddWithValue("id", textBox2.Text);
             command.Parameters.AddWithValue("date", dateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss"));
             command.Parameters.AddWithValue("number", textBox3.Text);
             command.Parameters.AddWithValue("dateShipment", dateTimePicker2.Value.ToString("yyyy-MM-dd HH:mm:ss"));
             command.Parameters.AddWithValue("provider", comboBox1.SelectedItem);
             command.Parameters.AddWithValue("retailSum", textBox6.Text);
-            command.Parameters.AddWithValue("incomingSum", textBox4.Text);
+            //command.Parameters.AddWithValue("incomingSum", textBox4.Text);
             command.ExecuteNonQuery();
             conn.Close();
         }
@@ -74,21 +89,32 @@ namespace chuc_coursework
         private void button3_Click(object sender, EventArgs e)
         {
             listBox1.Items.Add($"номер: {textBox1.Text}, контрагент: {comboBox1.Text}, название: {comboBox2.Text}, количество: {textBox3.Text}");
-        }
-        
-        private void label16_Click(object sender, EventArgs e)
-        {
-            
+            decimal colich = Convert.ToDecimal(textBox3.Text);
+            decimal prih = Convert.ToDecimal(textBox4.Text);
+            decimal nd = Convert.ToDecimal(comboBox3.Text);
+            switch (nd)
+            {
+                case 10:
+                    decimal nan = colich * (prih * 1.1m);
+                    label8.Text = Convert.ToString(nan);
+                    decimal nazen = Convert.ToDecimal(textBox5.Text);
+                    decimal m = nan + nazen;
+                    label14.Text = Convert.ToString(m);
+                    break;
+                case 20:
+                    decimal na = colich * (prih * 1.2m);
+                    label8.Text = Convert.ToString(na);
+                    decimal naze = Convert.ToDecimal(textBox5.Text);
+                    decimal mm = na + naze;
+                    label14.Text = Convert.ToString(mm);
+                    break;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int pz = Convert.ToInt32(textBox4.Text);
-            int nds = Convert.ToInt32(textBox8.Text);
-            int naz = Convert.ToInt32(textBox5.Text);
-            
-            decimal vsego = Convert.ToInt32(textBox6.Text) * (pz + nds + naz);
-            label16.Text = Convert.ToString(vsego);
+            string d = textBox1.Text + comboBox1.Text + comboBox2.Text + textBox7.Text + textBox3.Text + textBox4.Text + comboBox3.Text + textBox5.Text;
+            d = string.Empty;
         }
     }
 }
